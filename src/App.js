de940bf8377f566adbc724bb4927a57d180abde0
Arun <ajithrohitsachin20@gmail.com>
2019-07-12 18:17:49 +0530

    Added aws cognito credentials

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import React from 'react';
 
 function App() {
 
-  const [email] = useInput('');
-  const [password] = useInput('');
-
   return (
     <>
-      <input {...email} />
+      
     </>
   );
 }
---
+++ src/aws-config.js
@@ -0,0 +1,7 @@
+export default {
+    cognito: {
+        REGION: 'us-east-2',
+        USER_POOL_ID: 'us-east-2_jxojPxJk6',
+        APP_CLIENT_ID: '4gc1knflj03958pv56h9n3ktsr'
+    }
+};
