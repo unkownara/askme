d6e773b9a6055816043736b80922ad9b0af91ca3
Arun <ajithrohitsachin20@gmail.com>
2019-07-12 17:05:11 +0530

    custom hooks for input values

---
+++ src/components/hooks/useInput.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+export function useInput({ initialValue }) {
+    const [value, setValue] = useState(initialValue);
+    function onChangeHandler(e) {
+        setValue(e.target.value);
+    }
+    return {
+        value,
+        onChange: onChangeHandler
+    };
+}
