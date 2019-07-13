9c5cf0c638985a72c97f56fec74693cbd00c454b
aravindmv97 <aravindmv97@gmail.com>
2019-07-13 15:29:39 +0530

    Routes and containers added.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
 
-function App() {
+import history from './history';
+import NavBar from './components/NavBar';
+import LandingPage from './containers/LandingPage';
+import WallPage from './containers/WallPage';
 
-  return (
-    <>
-      
-    </>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router history={history}>
+        <NavBar />
+        <Switch>
+          <Route
+            path="/"
+            exact
+            component={LandingPage} />
+          <Route
+            path="/wall"
+            exact
+            component={WallPage} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/containers/WallPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+class WallPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+WallPage.propTypes = {};
+
+export default WallPage;
