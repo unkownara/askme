ba021a975b97b6f3a7917651ac3588a9399345eb
Ajith <ajithsachinrohit@gmail.com>
2019-07-14 01:28:47 -0400

    authentication validation in signup page

--- src/App.js
+++ src/App.js
@@ -2,27 +2,30 @@ import React from 'react';
 import createBrowserHistory from './history';
 import { Router, Switch, Route } from 'react-router';
 import { LoginPage } from './components/authentication/LoginPage'
-import { LandingPage } from './components/landingPage/LandingPage';
+// import { LandingPage } from './components/landingPage/LandingPage';
+import { Signup } from './components/authentication/Signup';
 
 function App() {
 	return (
-		<div className="App">
+		<div className="loginForm" >
 			<Router history={createBrowserHistory}>
+				<div className="textField">
+					<p>login</p>
+				</div>
 				<Switch>
 					<Route
 						exact
-						path="/home"
+						path="/"
 						component={LoginPage}
 					/>
 					<Route
 						exact
-						path="/"
-						component={LandingPage}
+						path="/signup"
+						component={Signup}
 					/>
 				</Switch>
 			</Router>
 		</div>
 	)
 }
-
-export default App;
+export default App; 
