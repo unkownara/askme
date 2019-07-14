87cf7f73052b13ef75335aea786adc8d73ca9bfe
Ajith <ajithsachinrohit@gmail.com>
2019-07-14 06:59:18 -0400

    history conflicts resolved

--- src/history.js
+++ src/history.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 import { createBrowserHistory } from "history";
 const history = createBrowserHistory();
 export default history; 
-=======
-import { createBrowserHistory } from 'history'
-const history = createBrowserHistory();
-export default history
->>>>>>> d74d60b1cdbd9af574daaaa70ffcef02d219dfc0
