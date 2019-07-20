470c59bbf1915e46566294cd5aea5dc438a8f8b6
aravindmv97 <aravindmv97@gmail.com>
2019-07-20 22:33:46 +0530

    Minor fix

--- src/containers/WallPage.js
+++ src/containers/WallPage.js
@@ -1,17 +1,12 @@
-import React, { useState, useEffect, lazy, Suspense } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import history from '../history';
 import { AskQuestionBox } from '../components/AskQuestionBox';
 import { ProfileCard } from '../components/ProfileCard';
 import { PopularUsers } from '../components/PopularUsersCard';
 import { PopularQuestions } from '../components/PopularQuestionsCard';
-import { FallbackLoader } from '../components/SusponseFallbackLoader';
-const Feed = lazy(() =>
-  import('../components/mainFeed/Feed').then(module => ({ default: module.Feed }))
-);
-// const Feed = lazy(() => import('../components/mainFeed/Feed'));
+
 
 const popularUsersData = [
     {
@@ -33,70 +28,39 @@ const popularQuestionsData = [
 
 export function WallPage() {
 
-    const [canRender, setRender] = useState(false);
+    const reduxStore = useSelector(state => state.userReducer);
+    
+    useEffect(() => {
+        console.log('redux store ', reduxStore);
+    });
 
-    const storeUser = useSelector(state => state.userReducer);
-    const dispatch = useDispatch();
-
-    // Checking if user info is exists or not in local storage.
-    // useEffect(() => {
-    //     const localStorageUserInfo = JSON.parse(localStorage.getItem('_user_info_'));
-    //     if(localStorageUserInfo === null || localStorageUserInfo === undefined) {
-    //         history.push('/login');
-    //     } else {
-    //         setRender(true);
-    //         dispatch({
-    //             type: 'STORE_USER_INFORMATION',
-    //             payload: localStorageUserInfo
-    //         })
-    //     }
-    // }, [canRender]);
-
-
-    // if(storeUser === null || storeUser.userInfo === null) {
-    //     return (
-    //         <>
-    //             Loading...
-    //         </>
-    //     );
-    // } else {
-    //     const userInfo = storeUser.userInfo;
-        return (
-            <>
-                <PageWrapper>
-                    <RowOneWrapper>
-                        <ProfileCard
-                            userFullName={'Aravind Manoharan'}
-                            username={'@aravindmv97'}
-                            askedCount={'100'}
-                            answeredCount={'200'} />
-                        <PopularUsers
-                            margin={'40px auto'}
-                            popularUsers={popularUsersData} />
-                    </RowOneWrapper>
-                    <RowtwoWrapper>
-                        <AskQuestionBox 
-                            username={'Aravind Manoharan'} 
-                            // userInfo={userInfo}
-                        />
-                        <Suspense fallback={<FallbackLoader />}>
-                            <Feed
-                                userId={userInfo.userId}
-                            />
-                        </Suspense>
-                    </RowtwoWrapper>
-                    <RowThreeWrapper>
-                        <PopularQuestions popularQuestions={popularQuestionsData} />
-                    </RowThreeWrapper>
-                </PageWrapper>
-            </>
-        )
-    }
-
+    return (
+        <>
+            <PageWrapper>
+                <RowOneWrapper>
+                    <ProfileCard
+                        userFullName={'Aravind Manoharan'}
+                        username={'@aravindmv97'}
+                        askedCount={'100'}
+                        answeredCount={'200'} />
+                    <PopularUsers
+                        margin={'40px auto'}
+                        popularUsers={popularUsersData} />
+                </RowOneWrapper>
+                <RowtwoWrapper>
+                    <AskQuestionBox />
+                </RowtwoWrapper>
+                <RowThreeWrapper>
+                    <PopularQuestions popularQuestions={popularQuestionsData} />
+                </RowThreeWrapper>
+            </PageWrapper>
+        </>
+    )
+}
 
 const PageWrapper = styled.div`
     display: grid;
-    grid-template-columns: 1fr 3fr 1fr;
+    grid-template-columns: 1.5fr 2fr 1.5fr;
 `;
 
 const RowOneWrapper = styled.div`
